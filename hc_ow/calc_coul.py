4c4071bf1ef1f957d7bf15e4d52d033f7d198181
Nick Rego <nrego2@gmail.com>
2017-05-17 12:09:46 -0400

    png files

--- hc_ow/calc_coul.py
+++ hc_ow/calc_coul.py
@@ -4,7 +4,7 @@ from __future__ import division, print_function
 
 import MDAnalysis
 import numpy as np
-
+import itertools
 import argparse
 try:
     u_for_prev = u_for
@@ -22,15 +22,24 @@ atmtypes = [('DUM_HC', 'DUM_HC'),
             ('DUM_OW_spc', 'DUM_OW_spc')]
 
 
-charges = [(0.0, 0.5),
-           (0.0, -0.5)]
+charges = [0.1,-0.1]
 
 
 ke = 138.9354859 # conv factor to get kJ/mol
 
 fudge = 1.0
 
+box = univ.dimensions[:3] / 10.0
 
+n_shifts = 1
+shifts = []
+print("generating shift vectors for n_shift: {}!".format(n_shifts))
+for n_shift in range(n_shifts+1):
+    shifts.append(-n_shift)
+    shifts.append(n_shift)
+shifts = np.unique(shifts)
+shift_vectors = np.array([p for p in itertools.product(shifts, repeat=3)])
+print("  ...{} shift vectors generated!".format(shift_vectors.shape[0]))
 
 n_frames = univ.trajectory.n_frames
 my_diffs = np.zeros((n_frames, 2))
@@ -41,37 +50,22 @@ for i_frame in range(n_frames):
     univ.trajectory[i_frame]
     # Get positions in nm
     univ.atoms.positions = univ.atoms.positions / 10.0
-    coul_a = 0.0
-    coul_b = 0.0
-    
+    coul = 0.0
+
+    atm_i = univ.atoms[0]
+    atm_j = univ.atoms[1]
+
+    pos_i = atm_i.position
+    pos_j = atm_j.position
+
     my_diffs[i_frame, 0] = univ.trajectory.time
 
-    for i in alc_indices:
-        atm_i = univ.atoms[i]
+    for this_shift in shift_vectors:
+        shift = this_shift * box
+        r = np.linalg.norm(pos_i - pos_j+shift)
+        coul += ke*(0.01)*(1/r)
+        if not np.array_equal(shift, np.zeros(3)):
+            coul += 2*ke*(-0.01)*(1/np.linalg.norm(shift))
 
-        i_type_a, i_type_b = atmtypes[i]
-        assert atm_i.type == i_type_a
-
-        i_charge_a, i_charge_b = charges[i]
-        assert atm_i.charge == i_charge_a
-
-        for j in alc_indices:
-            if j <= i:
-                continue
-
-            atm_j = univ.atoms[j]
-
-            j_type_a, j_type_b = atmtypes[j]
-            assert atm_j.type == j_type_a
-
-            j_charge_a, j_charge_b = charges[j]
-            assert atm_j.charge == i_charge_a
-
-            r_sq = np.sum((atm_i.position - atm_j.position)**2)
-
-            coul_a += ke*fudge*((i_charge_a*j_charge_b)/(r_sq))
-            coul_b += ke*fudge*((i_charge_b*j_charge_b)/(r_sq))
-
-
-    my_diffs[i_frame, 1] = coul_b - coul_a
+    my_diffs[i_frame, 1] = coul
 
